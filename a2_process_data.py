53b08368ed8094b6447f124a8c6b232d888aac79
Karaketir16 <karaketir16@itu.edu.tr>
2017-12-02 00:24:26 +0300

    a2_output.html is ready delete from 2011 to 2016

--- a2_process_data.py
+++ a2_process_data.py
@@ -18,43 +18,106 @@ with open("a2_input.csv") as input_file:
 ### of how to access the data.
 ### Print your results using the print function.
 #######################################################
-print("<!DOCTYPE html><html lang=\"en\"><head><meta charset=\"utf-8\"><title>India Temperature Analysis</title></head><body>")
+#print("the variable contents: ")
+#print(contents)
+#print("the variable contents[0]")
+#print(contents[0])
+#print("the variable contents[0][0]")
+#print(contents[0][0])
+#print("the type of the variable contents using the type() function: ")
+#print(type(contents))
+#print("the type of the variable contents[0]: ")
+#print(type(contents[0]))
+#print("the type of the variable contents[0][0]: ")
+#print(type(contents[0][0]))
+#print("the variable contents[5][6]")
+#print(contents[5][6])
+#print("the type of the variable contents[5][6]: ")
+#print(type(contents[0][0]))
+#print("population of Turkey in 2000: ")
+#totalPopulation =0
+#for i in range(1,82):
+#	totalPopulation=totalPopulation+int(contents[i][1])
+#print(totalPopulation)
+#print("average population of cities in Turkey in 2001: ")
+#totalPopulation =0
+#for i in range(1,82):
+#	totalPopulation=totalPopulation+int(contents[i][2])
+#averageIn2001=totalPopulation/81
+#print(round(averageIn2001,0))
+#conc=contents[1][0]+contents[2][0]
+#print(conc)
+#print(contents["hello"])
+#conc=contents[1][0]+contents[1][1]
+#print(conc)
+#print(contents[1][0]*3)
+#print(type(conc),type(contents[1][0]*3),type(averageIn2001))
+#help(conc)
+#help(averageIn2001)
+html = """
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8"/>
+        <title>population of Turkey from 2000 to 2010</title>
+        <style>
+            body{
+            font-size: 15px;
+            }
+            table, th, td {
+                border: 1px solid black;
+                border-collapse: collapse;
+            }
+            th, td {
+                padding: 5px;
+            }
+            td{
+            text-align: right;
+            }
+            .grey{
+            background-color: #dddddd;
+            }
+        </style>
+    </head>
+    <body>
+        <table>
+            %s
+        </table>
+    </body>
+</html>
+"""
 
-print("<p>the variable contents: </p>")
-print(contents)
-print("<p>the variable contents[0]</p>")
-print(contents[0])
-print("<p>the variable contents[0][0]</p>")
-print(contents[0][0])
-print("<p>the type of the variable contents using the type() function: </p>")
-print(type(contents))
-print("<p>the type of the variable contents[0]: </p>")
-print(type(contents[0]))
-print("<p>the type of the variable contents[0][0]: </p>")
-print(type(contents[0][0]))
-print("<p>the variable contents[5][6]")
-print(contents[5][6])
-print("<p>the type of the variable contents[5][6]: </p>")
-print(type(contents[0][0]))
-print("<p>population of Turkey in 2000: </p>")
-totalPopulation =0
-for i in range(1,82):
-	totalPopulation=totalPopulation+int(contents[i][1])
-print(totalPopulation)
-print("<p>average population of cities in Turkey in 2001: </p>")
-totalPopulation =0
-for i in range(1,82):
-	totalPopulation=totalPopulation+int(contents[i][2])
-averageIn2001=totalPopulation/81
-print(round(averageIn2001,0))
-conc=contents[1][0]+contents[2][0]
-print(conc)
-print("<p>print(contents[\"hello\"]): TypeError: list indices must be integers or slices, not str</p>")
-conc=contents[1][0]+contents[1][1]
-print(conc)
-print(contents[1][0]*3)
-print(type(conc),type(contents[1][0]*3),type(averageIn2001))
-help(conc)
-help(averageIn2001)
+include=""
+th="<th>%s</th>"
+td="<td>%s</td>"
+i=0
+for row in contents:
+    include+="<tr>\n"
+    a=0
+    color=0
+    for cell in contents[i]:
 
-print("</body></html>")
+        if(i==0 or a==0):
+            co="<th class=\"%s\">"
+            if(color%2==0):
+                include+=co%"grey"
+            else:
+                include+=co%"g"
+            include+=cell
+            include+="</th>\n"
+            color+=1
+        else:
+            co="<td class=\"%s\">"
+            if(color%2==0):
+                include+=co%"grey"
+            else:
+                include+=co%"g"
+            include+=cell
+            include+="</td>\n"
+            color+=1
+        a+=1
+    i+=1
+    include+="</tr>\n"
+
+
+print(html%include)
